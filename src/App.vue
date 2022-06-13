e972e2be412af1f73e61f362a085819af506592d
José Carlos <etijcvivi@gmail.com>
2022-06-12 21:07:55 -0300

    Projeto todolist Adicionando SASS

--- src/App.vue
+++ src/App.vue
@@ -11,24 +11,5 @@ export default {
 </script>
 
 <style>
-	body {
-		font-family: 'Lato', sans-serif;
-		background: linear-gradient(to right, #43c6ac, #191654);
-		color: #FFF;
-	}
-
-	#app {
-		display: flex;
-		flex: 1;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-	}
-
-	#app h1 {
-		margin-bottom: 5px;
-		font-weight: 300;
-		font-size: 3rem;
-	}
+	@import './estilosToDoList/app.sass';
 </style>
