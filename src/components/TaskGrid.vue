f02ca6b7a6d28301dc322e3df610c688db95b70e
José Carlos <etijcvivi@gmail.com>
2022-06-12 21:50:37 -0300

    Adicionando Tasks de forma Manual Teste

--- src/components/TaskGrid.vue
+++ src/components/TaskGrid.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="tasks">
-        <template>
+        
             <div v-for="task in tasks" :key="task.name">{{ task.name }}</div>
-        </template>
+        
     </div>
 
 </template>
