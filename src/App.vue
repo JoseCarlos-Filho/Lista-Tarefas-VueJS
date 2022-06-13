74485a0b71727f1211ef3223bd8a1462205a3c4e
José Carlos <etijcvivi@gmail.com>
2022-06-12 21:41:54 -0300

    Componente TaskGrid criado

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+    <TaskGrid :tasks="tasks"/>
 	</div>
 </template>
 
 <script>
-export default {
+import TaskGrid from './components/TaskGrid.vue';
 
+export default {
+  components: { TaskGrid },
+  // criando tarefas de forma manual.
+  data() {
+    return {
+      tasks: [
+        { name: 'Lavar a louça', pending: false },
+        { name: 'Comprar blusa', pending: true},
+      ]
+    }
+  }
 }
 </script>
 
