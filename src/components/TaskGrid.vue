2cf010b25dc0ef41bdf9a2b4e980b0c055c5f51a
José Carlos <etijcvivi@gmail.com>
2022-06-13 21:28:58 -0300

    Adicionando componente de tarefas TaskList.vue

--- src/components/TaskGrid.vue
+++ src/components/TaskGrid.vue
@@ -1,20 +1,18 @@
 <template>
     <div class="task-grid">
             <!-- v-for gera as divs com as tarefas.-->
-            <TaskList v-for="task in tasks" 
-            :key="task.name" 
-            :task="task" ></TaskList>
+            <TaskList v-for="task in tasks" :key="task.name" :task="task"></TaskList>
         
     </div>
 
 </template>
 
 <script>
-import TaskList from './TaskList.vue'
+import TaskList from './TaskList.vue';
 export default {
     componets: { TaskList },
     props: {
-        tasks: { type: Array, required: true },
+        tasks: { type: Array, required: true }
     }
 }
 
